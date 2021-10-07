80754c24dd0c27ede3c3ee9a5bd8bc07be3c01e5
Chris Hall <mrchrislhall@gmail.com>
2021-10-07 11:02:02 -0700

    Plug a couple things in. Might start over completely, dunno

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-// Hokay. So here are the categories I want to have chosen for me:
-// Stuff off my to-do list. Skill development. Projects.
-// I might want to do two things on one day, but PROLY not.
 
-// On a day, I want to choose between each category. That category will
-// be assigned for the rest of the day. (each one tracks last time it was
-// chosen too). I can either click a category or click "Choose for me!".
-// (it might be nice to configure how choose for me works, random? or
-// cycling through each one every day?)
-// Once I have picked a category for the day, I am presented with the
-// the projects in it, as well as the last time I worked on each. I
-// either click a project to choose it, or click "Choose for me" which
-// always chooses randomly. Then, the project is shown for the rest of
-// the day whenever you return to the page.
-
-// In the configuration page. I can add or remove categories, up to 4
-// (arbitrary). I can add or remove projects from each, also up to 4.
-// I can mark projects as done, and put them in the done list.
-// I can configure at what time each day the system resets (3AM).
-
-// There is a work section and an entertainment section. It would be
-// nice to fully separate them.
-
-// It would be nice to sync across devices blah blah blah but it must
-// be lightweight, and I'm going to actually try using it before I worry
-// about that. It would be nice to pull entertainment recommendations
-// out of something random like popular Itch games for example. It
-// might be nice to have a log of everything worked on - but this isn't
-// for time tracking. It might be nice to be able to just type in what
-// youre doing for the day - but if you already know what to do today
-// you shouldn't use this today. It might be nice to have a place where
-// I can just type a comma separated list, and have it pick from there
-// and stay picked for the day.
-
-// Hmm. Maybe don't keep any record of what you worked on. This is to
-// avoid being precious about projects etc, I can track that separately.
-
-// hokay LETS GO.
 
 const PRODUCTIVITY = 0;
 const ENTERTAINMENT = 1;
@@ -46,7 +9,7 @@ function populate(elem, arr) {
 }
 
 // might be nice to build this object from a list of strings
-let productivityOptions = [
+let project_options = [
   {
     category: 'Programming projects',
     members: [
@@ -82,7 +45,7 @@ let productivityOptions = [
 
 let doneProductivity = [];
 
-let entertainmentOptions = [
+let entertainment_options = [
   {
     category: 'Movies',
     members: [
